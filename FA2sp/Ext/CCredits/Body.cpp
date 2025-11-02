af54ad6fe58914c760a1f4f15f8180571c3a0f19
RAZER <79311432+CnCRAZER@users.noreply.github.com>
2025-11-02 01:15:17 -0500

    Remove CNewEasterEgg feature and related files
    Deleted the CNewEasterEgg implementation (CNewEasterEgg.cpp, .h, .rc) and removed all references to it from the project files and CCredits extension. This eliminates the easter egg games (GoBang and Chinese Chess) from the application.

--- FA2sp/Ext/CCredits/Body.cpp
+++ FA2sp/Ext/CCredits/Body.cpp
@@ -1,5 +1,4 @@
 #include "Body.h"
-#include "../../ExtraWindow/CNewEasterEgg/CNewEasterEgg.h"
 
 CCredits* CCreditsExt::Instance = nullptr;
 
@@ -8,32 +7,8 @@ void CCreditsExt::ProgramStartupInit()
 	RunTime::ResetMemoryContentAt(0x592810, &CCreditsExt::PreTranslateMessageExt);
 }
 
-static int loop = -1;
 BOOL CCreditsExt::PreTranslateMessageExt(MSG* pMsg)
 {
-	if (this->DrawHappyFace && pMsg->message == WM_LBUTTONUP)
-	{
-		if (loop == -1)
-		{
-			loop = STDHelpers::RandomSelectInt(0, 2);
-		}
-		if (loop % 2 == 0)
-		{
-			if (CChineseChess::GetHandle() == NULL)
-				CChineseChess::Create(CFinalSunDlg::Instance);
-			loop++;
-		}
-		else
-		{
-			if (CGoBang::GetHandle() == NULL)
-				CGoBang::Create(CFinalSunDlg::Instance);
-			loop++;
-		}
-		return TRUE;
-	}
-	else if (this->DrawHappyFace && pMsg->message == WM_LBUTTONDOWN)
-	{
-		return TRUE;
-	}
+	// Remove all easter-egg game triggers (CChineseChess/CGoBang) and fall back to default handling
 	return this->ppmfc::CDialog::PreTranslateMessage(pMsg);
 }
